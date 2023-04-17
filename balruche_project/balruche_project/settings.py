83aa01b9956880e5b45b04ef942d3534efb778a7
JLC <jlc@orange.fr>
2023-04-17 21:52:03 +0200

    Update settings

--- balruche_project/balruche_project/settings.py
+++ balruche_project/balruche_project/settings.py
@@ -30,7 +30,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 127.0.0.1]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'cjlux.fr']
 
 
 # Application definition
