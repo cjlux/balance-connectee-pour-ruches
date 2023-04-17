13327cbd1c30ad92417e838621f2366ba4f29c3d
Jean-Luc Charles <jean-luc.charles@ensam.eu>
2023-04-17 21:20:59 +0200

    update settings

--- balruche_project/balruche_project/settings.py
+++ balruche_project/balruche_project/settings.py
@@ -104,14 +104,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_TZ = True
-
+LANGUAGE_CODE = 'fr-fr'
+TIME_ZONE = 'Europe/Paris'
+USE_I18N  = True
+USE_TZ    = True
+DATE_INPUT_FORMATS = ('%d/%m/%Y','%Y-%m-%d')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
