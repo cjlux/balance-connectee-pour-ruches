cf8938271df0ca2b8e20f80f6e5889b946972095
Jean-Luc Charles <jean-luc.charles@ensam.eu>
2023-04-18 16:42:13 +0200

    add r'^$' to urlpatterns

--- balruche_project/balruche_project/urls.py
+++ balruche_project/balruche_project/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
 from balruche import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', views.hello, name='hello'),
+    re_path(r'^$', views.hello, name='hello'),
 ]
