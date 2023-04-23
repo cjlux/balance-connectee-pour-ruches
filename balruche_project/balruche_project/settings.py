6ea5dfe8bb4e367aa9ddcbf3c9d3e1e7b3591277
Jean-Luc Charles <jean-luc.charles@ensam.eu>
2023-04-23 19:31:02 +0200

    remove widget-tweaks

--- balruche_project/balruche_project/settings.py
+++ balruche_project/balruche_project/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     'widget_tweaks',
     'authentication',
     'balruche',
 ]
